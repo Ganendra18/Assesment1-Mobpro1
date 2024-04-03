180ebd0d3b58b742ca2a4ab834686b7b176a8558
Ganendra Kalla Radhya <funtastic@student.telkomuniversity.ac.id>
2024-04-03 09:44:11 +0700

    Memberi pilihan kepada pengguna & Menampilkan hasil pilihan

--- app/src/main/java/org/d3if0122/assesment1mobpro/ui/screen/MainScreen.kt
+++ app/src/main/java/org/d3if0122/assesment1mobpro/ui/screen/MainScreen.kt
@@ -1,5 +1,6 @@
 package org.d3if0122.assesment1mobpro.ui.screen
 
+import android.content.Context
 import android.content.res.Configuration
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
@@ -33,6 +34,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.text.input.ImeAction
@@ -69,16 +71,45 @@ fun ScreenContent(modifier: Modifier){
     var nama by remember { mutableStateOf("") }
     var lamaHari by remember { mutableStateOf("") }
 
+    var hasilText by remember { mutableStateOf("") }
+    val context = LocalContext.current
+
+    fun getMotorOptionsText(context: Context, jenis: String, merk: String):String {
+        val resources = context.resources
+        val motorStringResId = when (jenis) {
+            resources.getString(R.string.honda) -> {
+                when (merk) {
+                    resources.getString(R.string.matic) -> R.string.motor_honda_matic
+                    resources.getString(R.string.bebek) -> R.string.motor_honda_bebek
+                    resources.getString(R.string.kopling) -> R.string.motor_honda_kopling
+                    else -> -1 // Handle error
+                }
+            }
+            resources.getString(R.string.yamaha) -> {
+                when (merk) {
+                    resources.getString(R.string.matic) -> R.string.motor_yamaha_matic
+                    resources.getString(R.string.bebek) -> R.string.motor_yamaha_bebek
+                    resources.getString(R.string.kopling) -> R.string.motor_yamaha_kopling
+                    else -> -1 // Handle error
+                }
+            }
+            else -> -1 // Handle default case or error
+        }
+        return if (motorStringResId != -1){
+            resources.getString(motorStringResId)
+        } else {
+            "Error: Tipe Motor tidak ada Mas Bro!"
+        }
+    }
+
     val radioOptions = listOf(
         stringResource(id = R.string.honda),
-        stringResource(id = R.string.yamaha),
-        stringResource(id = R.string.suzuki)
+        stringResource(id = R.string.yamaha)
     )
     var merk by remember { mutableStateOf(radioOptions[0]) }
 
     var isExpanded by remember { mutableStateOf(false) }
     var selectedText by remember { mutableStateOf(list[0]) }
-
 
     Column (
         modifier = modifier
@@ -111,7 +142,7 @@ fun ScreenContent(modifier: Modifier){
             singleLine = true,
             keyboardOptions = KeyboardOptions(
                 keyboardType = KeyboardType.Number,
-                imeAction = ImeAction.Next
+                imeAction = ImeAction.Done
             ),
             modifier = Modifier.fillMaxWidth()
         )
@@ -143,7 +174,9 @@ fun ScreenContent(modifier: Modifier){
         }
 
         Row (
-            modifier = Modifier.padding(top = 6.dp).border(1.dp, Color.Gray, RoundedCornerShape(4.dp))
+            modifier = Modifier
+                .padding(top = 6.dp)
+                .border(1.dp, Color.Gray, RoundedCornerShape(4.dp))
         ){
             radioOptions.forEach { text ->
                 PilihanMotor(
@@ -151,25 +184,31 @@ fun ScreenContent(modifier: Modifier){
                     isSelected = merk == text,
                     modifier = Modifier
                         .selectable(
-                        selected = merk == text,
-                        onClick = { merk = text },
-                        role = Role.RadioButton
-                    )
+                            selected = merk == text,
+                            onClick = { merk = text },
+                            role = Role.RadioButton
+                        )
                         .weight(1f)
                         .padding(16.dp)
                 )
             }
         }
         Button(
-            onClick = {  },
+            onClick = { hasilText = getMotorOptionsText(context, merk, selectedText) },
             modifier = Modifier.padding(top = 8.dp),
             contentPadding = PaddingValues(horizontal = 32.dp, vertical = 16.dp)
         ) {
-            Text(text = stringResource(id = R.string.tombol_cari))
+            Text(text = stringResource(id = R.string.tombol_print))
         }
+        Text(
+            text = hasilText,
+            style = MaterialTheme.typography.bodyLarge,
+            modifier = Modifier.fillMaxWidth()
+        )
     }
 
 }
+
 
 @Composable
 private fun PilihanMotor(label: String, isSelected: Boolean, modifier: Modifier){
@@ -180,7 +219,7 @@ private fun PilihanMotor(label: String, isSelected: Boolean, modifier: Modifier)
         RadioButton(selected = isSelected, onClick =  null)
         Text(
             text = label,
-            style = MaterialTheme.typography.bodyMedium,
+            style = MaterialTheme.typography.bodyLarge,
             modifier = Modifier.padding(start = 8.dp)
         )
     }
